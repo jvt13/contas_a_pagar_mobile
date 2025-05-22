9d1a336642ebabf9f8982e7867745de4b8e175a7
jvt13 <jvocse@gmail.com>
2025-05-22 16:46:47 -0400

    corrigido bug de conexao com o servidor

--- app.config.js
+++ app.config.js
@@ -1,9 +1,12 @@
-import 'dotenv/config'; // se usar .env
+import 'dotenv/config'; // só se usar .env
 
 export default ({ config }) => ({
   ...config,
   extra: {
-    ...config.extra,                           // mantém eas.projectId
-    EXPO_PUBLIC_API_URL:'http://192.168.15.100:5000'             // ou fallback local
-  }
+    ...config.extra, // mantém eas.projectId
+    // Aqui sim, usamos a env do EAS ou, em dev, caímos no fallback local
+    EXPO_PUBLIC_API_URL:
+      process.env.EXPO_PUBLIC_API_URL ||
+      'http://192.168.15.100:5000',
+  },
 });
